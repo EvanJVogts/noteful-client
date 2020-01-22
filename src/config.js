8a6a1c90f6e1dfc8f511a09080ce00b18b2ccd3e
Evan Vogts <evanjvogts@gmail.com>
2020-01-22 15:50:55 -0600

    deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 export default {
-  API_ENDPOINT: 'https://morning-island-14729.herokuapp.com',
-  API_KEY: process.env.REACT_APP_API_KEY,
+  API_ENDPOINT: 'https://morning-island-14729.herokuapp.com/api',
 }
